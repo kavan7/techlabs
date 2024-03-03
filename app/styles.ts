088d6fa39efc1ed95395126947087937c814866c
kavan7 <abeyratnekavan@gmail.com>
2024-03-02 20:40:49 -0500

    first

---
+++ app/styles.ts
@@ -0,0 +1,18 @@
+
+const styles = {
+    paddingX: "sm:px-16 px-6",
+    paddingY: "sm:py-16 py-6",
+    padding: "sm:px-16 px-6 sm:py-16 py-10",
+  
+    heroHeadText:
+      "font-black text-center text-[#005daa] lg:text-[62px] sm:text-[30px] xs:text-[42px] text-[37.4px] leading-[53px] sm:leading-[50px] sm:mb-18 mb-0 ",
+    heroSubText:
+      "text-[#005daa] font-medium lg:text-[33px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+  
+    sectionHeadText:
+      "text-[#005daa] font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+    sectionSubText:
+      "sm:text-[28px] text-[18px] text-secondary uppercase tracking-wider",
+  };
+  
+  export { styles };
